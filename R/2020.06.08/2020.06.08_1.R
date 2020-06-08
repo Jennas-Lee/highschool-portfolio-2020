a2c1bf3c45c53f0192189af08ac2baa45bd0ba28
iamseungjun <leezeunjun@gmail.com>
2020-06-08 12:39:31 +0900

    2020.06.08 R

---
+++ R/2020.06.08/2020.06.08_1.R
@@ -0,0 +1,17 @@
+# 2020.06.08
+# 20617 이승준
+
+english <- c(90, 80, 60, 70)
+english
+
+math <- c(50, 60, 100, 20)
+math
+
+cls <- c(1, 2, 3, 4)
+cls
+
+df_midterm <- data.frame(english, math, cls)
+df_midterm
+
+mean(english) # english_avg
+mean(math)    # math_avg
