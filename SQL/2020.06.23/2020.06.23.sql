cbb59f4b4c374205f4cf80ec5446d96956faa9fc
Jennas Lee/이승준 <55793046+iamseungjun@users.noreply.github.com>
2020-06-23 08:48:59 +0900

    Create 2020.06.23.sql

---
+++ SQL/2020.06.23/2020.06.23.sql
@@ -0,0 +1,114 @@
+#
+# TABLE STRUCTURE FOR: CLASS
+#
+
+DROP TABLE IF EXISTS `CLASS`;
+
+CREATE TABLE `CLASS` (
+  `NAME` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `AGE` int(11) DEFAULT NULL,
+  `NO` int(11) NOT NULL,
+  `ADDRESS` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`NO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('molestias', 14, 0, '052 Connor Isle Apt.');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('odio', 18, 1, '0385 Dock Isle Apt. ');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('inventore', 19, 2, '6661 Green Lights\nEs');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('molestias', 17, 3, '1680 Wuckert Drive\nL');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('animi', 12, 5, '35837 O\'Reilly Oval\n');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('sed', 16, 6, '66536 Stark Island A');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('dolorem', 11, 7, '395 Domenick Fork Ap');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('et', 16, 8, '20130 Abigale Corner');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('ea', 17, 9, '1130 Koss Plaza Apt.');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('sed', 12, 22, '61280 Gina Lakes\nSou');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('numquam', 16, 23, '55046 Abbott Trace\nW');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('nesciunt', 12, 37, '631 Schaden Cliffs A');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('maxime', 24, 57, '3106 Morar Mall Apt.');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('sunt', 14, 65, '88777 Lizzie Radial ');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('rerum', 11, 73, '3783 Kuhlman Drive S');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('voluptas', 24, 79, '4680 Kenyon Hills Su');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('velit', 20, 80, '567 Heather Lights\nB');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('qui', 16, 96, '654 Ernie Junctions\n');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('recusandae', 23, 112, '457 Streich Plains\nP');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('non', 23, 343, '275 Bailey Port Suit');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('repellat', 18, 350, '77861 Dietrich Plain');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('non', 16, 393, '67726 Telly Village\n');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('sit', 18, 425, '369 Kuhn Key Suite 9');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('quis', 15, 492, '640 Mellie Viaduct\nS');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('in', 24, 583, '966 Wiza Brook Suite');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('est', 24, 616, '643 Walker Neck\nEast');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('tenetur', 13, 746, '00470 Aditya Skyway\n');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('occaecati', 16, 850, '8271 Juliana Trail S');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('dolores', 22, 855, '2771 Pinkie Plaza\nGi');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('recusandae', 16, 957, '69059 Cristina Vista');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('quasi', 17, 1546, '715 Wisoky Overpass\n');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('eaque', 14, 2104, '63807 Gisselle Plaza');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('quo', 15, 4303, '20535 Marcellus Lake');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('et', 15, 5142, '945 Retha Harbors Ap');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('dolore', 14, 5875, '8409 Durgan Fork\nSou');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('qui', 17, 5933, '0329 Roob Views\nMela');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('voluptatem', 21, 6124, '014 Heaney Trace Apt');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('nihil', 23, 6187, '2877 Goodwin Stream\n');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('incidunt', 24, 6245, '805 Herman Crest Apt');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('voluptas', 24, 6402, '1196 Mosciski Spurs\n');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('et', 16, 7335, '8715 Rohan Canyon Su');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('neque', 23, 8025, '3258 Armstrong Harbo');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('consequuntur', 19, 8152, '4950 Adams Springs A');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('ipsa', 20, 8797, '7368 Pagac Mountain ');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('aut', 12, 9171, '4718 Ladarius Flats\n');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('natus', 21, 9511, '911 Lemke Flats Suit');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('placeat', 12, 16426, '612 Bruen Trail\nPort');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('nesciunt', 18, 27856, '3450 Micah Squares A');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('quae', 22, 29973, '4030 Ottis Walks\nNew');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('molestiae', 16, 30094, '2438 Philip Viaduct ');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('est', 13, 30291, '6495 Ward Pass\nWest ');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('voluptatem', 15, 38514, '6369 Kenna Road\nDick');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('impedit', 17, 61495, '03991 Maryse Meadow ');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('laudantium', 23, 64255, '124 Mertz Cliffs\nSou');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('sed', 18, 121381, '15253 Padberg Pass S');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('enim', 17, 125964, '002 Meagan Stream\nNo');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('rerum', 14, 146786, '004 Boehm Ridge\nEast');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('rem', 19, 188176, '426 Wayne Orchard Su');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('odio', 13, 321381, '4274 Larkin Greens A');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('facere', 11, 374259, '90733 Katherine Lodg');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('et', 12, 489431, '71183 Sonny Shoal\nHe');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('neque', 18, 698012, '7319 Nicolas Tunnel\n');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('dicta', 18, 761463, '0212 Elliot Isle\nJos');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('reiciendis', 15, 833792, '881 Vivienne Pine Su');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('atque', 12, 838908, '241 Mante Squares\nNo');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('id', 22, 859536, '45145 Rodriguez Isla');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('iure', 21, 889885, '120 Hosea Ports Apt.');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('quisquam', 12, 984157, '3196 Mante Lake Apt.');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('magni', 22, 987655, '200 Dibbert Hollow A');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('veniam', 12, 1075097, '544 Pedro Springs Ap');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('laudantium', 17, 1425998, '20127 Ottis Ranch\nKe');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('repellat', 18, 4097792, '0496 Abshire Bypass ');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('dicta', 19, 6808311, '34802 Rachael Estate');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('dicta', 12, 6938925, '83183 Maggio Drive S');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('maxime', 19, 7386621, '1084 Seamus Mews\nPor');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('et', 19, 8785462, '79677 Dietrich Landi');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('est', 20, 9344381, '671 Friedrich Rest\nK');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('et', 17, 11116440, '19546 Agnes Road\nCar');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('reiciendis', 21, 21654256, '3639 Raynor Drives\nN');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('voluptas', 17, 59161167, '739 Annamarie Motorw');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('repellendus', 14, 70693431, '7962 Isobel Square S');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('assumenda', 18, 71946747, '85338 Arlo Villages ');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('aliquam', 21, 72861673, '50479 Bauch Lodge\nSo');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('non', 20, 86414656, '8081 Harvey Haven Ap');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('praesentium', 15, 95689741, '477 Kolby Rapid\nNew ');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('et', 21, 163413594, '3982 Claudie Parkway');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('culpa', 21, 176343284, '6503 Ruecker Village');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('et', 22, 277144882, '91560 Joey Springs\nS');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('officia', 15, 295519368, '312 Fermin Pine\nPort');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('et', 15, 298263388, '4878 Asia Ports\nMcDe');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('libero', 17, 433095434, '105 Glen Fort Suite ');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('illo', 19, 488574085, '45573 Trinity Mews\nE');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('quia', 15, 534838680, '13130 Abshire Mounta');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('consequatur', 18, 565682101, '770 Von Crossroad Su');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('voluptatem', 21, 565956441, '90793 Kemmer Corner\n');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('rerum', 17, 752239252, '6161 Feest Lodge Sui');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('ea', 15, 802718762, '90038 Dedrick Plains');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('quis', 14, 820938208, '09392 Braun Parkway ');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('aut', 20, 980015267, '074 Gavin Rest Suite');
+INSERT INTO `CLASS` (`NAME`, `AGE`, `NO`, `ADDRESS`) VALUES ('vel', 15, 991358310, '4253 Patience Pike\nP');
