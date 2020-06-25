9eec9760566abe82c7c5f6f71b7b8864463ad1ea
iamseungjun <leezeunjun@gmail.com>
2020-06-25 09:51:08 +0900

    2020.06.25 Java

---
+++ JAVA/2020.06.25/Test20200625_1.java
@@ -0,0 +1,9 @@
+                                                            
+public class Test20200625_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
