d90efb7bb58d05f45f3a4e013d296b0be4717d2a
iamseungjun <leezeunjun@gmail.com>
2020-06-25 11:20:34 +0900

    2020.06.25 Java

--- JAVA/2020.06.25/Test20200625_1.java
+++ JAVA/2020.06.25/Test20200625_1.java
@@ -1,9 +1,41 @@
-                                                            
+
+class Student {
+	
+	String name;
+	int grade;
+	int ban;
+	int number;
+	String telephone;
+	
+	void eat() {
+		System.out.println("냠냠");
+	}
+	void sleep() {
+		System.out.println("쿨쿨");
+	}
+	
+}
+
 public class Test20200625_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Student han = new Student();
+		han.name = "한승윤";
+		han.grade = 2;
+		han.ban = 6;
+		han.number = 25;
+		han.telephone = "010-0000-0000";
+		
+		han.eat();
+		han.sleep();
+		
+		System.out.println("학생의 이름은 " + han.name + "이고");
+		System.out.println("학생의 학년은 " + han.grade + "이고");
+		System.out.println("학생의 반은 " + han.ban + "이고");
+		System.out.println("학생의 번호는 " + han.number + "이고");
+		System.out.println("학생의 전화번호는 " + han.telephone + "이다.");
+		
 	}
 
 }
